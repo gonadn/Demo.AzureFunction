9b75f520657f207754857ae2d4d67fec14d60ae0
Adnan Riaz Gondal <gonadn@gmail.com>
2022-04-16 22:57:25 +0200

    Add project files.

---
+++ CreateCertificate.ps1
@@ -0,0 +1,4 @@
+﻿$cert = New-SelfSignedCertificate -Subject "CN=BaneNorMBA" -CertStoreLocation "Cert:\CurrentUser\My" -KeyExportPolicy Exportable -KeySpec Signature -KeyLength 2048 -KeyAlgorithm RSA -HashAlgorithm SHA256
+Export-Certificate -Cert $cert -FilePath "C:\adnan cert\BaneNorMBADev.cer"
+$mypwd = ConvertTo-SecureString -String "BnMBA2021!" -Force -AsPlainText
+Export-PfxCertificate -Cert $cert -FilePath "C:\BaneNorMBADev.pfx" -Password $mypwd
